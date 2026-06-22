43ab4841b9738cb4469cd6929e934255346c16eb
SaunakRath <saunaksiladtya2020@gmail.com>
2026-06-22 14:04:53 +0530

    Array linear search

--- Arrays/src/ArrayProblems.java
+++ Arrays/src/ArrayProblems.java
@@ -20,12 +20,27 @@ public class ArrayProblems {
         }
     }
 
+    public static boolean linearSearch(int[] arr, int target){
+        for (int i=0; i<arr.length; i++){
+            if (arr[i] == target){
+                return true;
+            }
+        }
+        return false;
+    }
+
     static void main() {
         int[] arr = {1, 2, 3, 4, 5};
+        boolean ans = linearSearch(arr,4);
+        System.out.println(ans);
+
+
+//        int[] arr = {1, 2, 3, 4, 5};
 //        System.out.println("Average : " + findAverage(arr));
 
-        multiply(arr);
-        System.out.println(Arrays.toString(arr));
+//        int[] arr = {1, 2, 3, 4, 5};
+//        multiply(arr);
+//        System.out.println(Arrays.toString(arr));
 
     }
 }

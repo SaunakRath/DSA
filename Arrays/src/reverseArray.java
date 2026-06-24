52507e5b8ba867ec2203b84ac6bab4870a5e10b1
SaunakRath <saunaksiladtya2020@gmail.com>
2026-06-24 13:49:31 +0530

    Reverse an Array

---
+++ Arrays/src/reverseArray.java
@@ -0,0 +1,24 @@
+public class reverseArray {
+    static void reversearr(int[] arr){
+        int n = arr.length;
+        int i = 0;
+        int j = n-1;
+
+        while(i < j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i++;
+            j--;
+        }
+        for(int k:arr){
+            System.out.println(k);
+        }
+    }
+
+    static void main() {
+        int[] arr = {1,2,3,4,5};
+        reversearr(arr);
+    }
+}
